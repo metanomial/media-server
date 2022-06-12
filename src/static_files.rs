f0a60b245d4ed051ace58c0762d66ab1520fdc6d
Benjamin Herman <benjamin@metanomial.com>
2022-06-11 18:54:03 -0700

    Remove unecessary async

--- src/static_files.rs
+++ src/static_files.rs
@@ -5,48 +5,48 @@ use std::include_bytes;
 use std::vec::Vec;
 
 #[rocket::get("/favicon.ico")]
-async fn favicon() -> (ContentType, &'static [u8]) {
+fn favicon() -> (ContentType, &'static [u8]) {
   let bytes = include_bytes!("../static/favicon.ico");
   (ContentType::Icon, bytes)
 }
 
 #[rocket::get("/favicon-16x16.png")]
-async fn favicon_16x16() -> (ContentType, &'static [u8]) {
+fn favicon_16x16() -> (ContentType, &'static [u8]) {
   let bytes = include_bytes!("../static/favicon-16x16.png");
   (ContentType::PNG, bytes)
 }
 
 #[rocket::get("/favicon-32x32.png")]
-async fn favicon_32x32() -> (ContentType, &'static [u8]) {
+fn favicon_32x32() -> (ContentType, &'static [u8]) {
   let bytes = include_bytes!("../static/favicon-32x32.png");
   (ContentType::PNG, bytes)
 }
 
 #[rocket::get("/apple-touch-icon.png")]
-async fn apple_touch_icon() -> (ContentType, &'static [u8]) {
+fn apple_touch_icon() -> (ContentType, &'static [u8]) {
   let bytes = include_bytes!("../static/apple-touch-icon.png");
   (ContentType::PNG, bytes)
 }
 
 #[rocket::get("/android-chrome-192x192.png")]
-async fn android_chrome_192x192() -> (ContentType, &'static [u8]) {
+fn android_chrome_192x192() -> (ContentType, &'static [u8]) {
   let bytes = include_bytes!("../static/android-chrome-192x192.png");
   (ContentType::PNG, bytes)
 }
 
 #[rocket::get("/android-chrome-512x512.png")]
-async fn android_chrome_512x512() -> (ContentType, &'static [u8]) {
+fn android_chrome_512x512() -> (ContentType, &'static [u8]) {
   let bytes = include_bytes!("../static/android-chrome-512x512.png");
   (ContentType::PNG, bytes)
 }
 
 #[rocket::get("/main.css")]
-async fn stylesheet() -> RawCss<&'static str> {
+fn stylesheet() -> RawCss<&'static str> {
   RawCss(include_str!("../static/main.css"))
 }
 
 #[rocket::get("/site.webmanifest")]
-async fn manifest() -> (ContentType, &'static str) {
+fn manifest() -> (ContentType, &'static str) {
   let text = include_str!("../static/site.webmanifest");
   (ContentType::new("application", "manifest+json"), text)
 }
